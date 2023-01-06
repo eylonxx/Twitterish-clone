2446289cd1596d95b3e284a71491b32da421d85e
Eylon <eylon.perets@gmail.com>
2023-01-06 14:12:50 +0200

    auth and user routes

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -26,3 +26,22 @@ export const register = async (req, res) => {
     res.status(500).json({ error: error.message });
   }
 };
+
+// LOGGING IN //
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) return res.status(400).json({ msg: 'User does not exist.' });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(400).json({ msg: 'Invalid credentials.' });
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    //delete pass so it doesnt get sent
+    delete user.password;
+    res.status(200).json({ token, user });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
--- server/index.js
+++ server/index.js
@@ -9,6 +9,8 @@ import morgan from 'morgan';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import { register } from './controllers/auth.js';
+import authRoutes from './routes/auth';
+import userRoutes from './routes/users';
 
 // CONFIGURATIONS //
 const __filename = fileURLToPath(import.meta.url);
@@ -37,6 +39,10 @@ const upload = multer({ storage });
 
 // ROUTES WITH FILES //
 app.post('/auth/register', upload.single('picture'), register);
+
+// ROUTES //
+app.use('/auth', authRoutes);
+app.use('/users', userRoutes);
 
 // MONGOOSE SETUP //
 const PORT = process.env.PORT || 6001;
