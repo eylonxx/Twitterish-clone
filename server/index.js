5bedc44ba74ebad81c04a574ef7ac71097636b24
Eylon <eylon.perets@gmail.com>
2023-01-08 09:42:05 +0200

    seed

--- server/index.js
+++ server/index.js
@@ -14,6 +14,9 @@ import userRoutes from './routes/users.js';
 import { register } from './controllers/auth.js';
 import { createPost } from './controllers/posts.js';
 import { verifyToken } from './middleware/auth.js';
+import User from './models/User.js';
+import Post from './models/Post.js';
+import { users, posts } from './data/index.js';
 
 // CONFIGURATIONS //
 const __filename = fileURLToPath(import.meta.url);
@@ -58,5 +61,9 @@ mongoose
   })
   .then(() => {
     app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
+
+    // SEED //
+    // User.insertMany(users);
+    // Post.insertMany(posts);
   })
   .catch((e) => console.log(`Error: ${e}, did not connect`));
