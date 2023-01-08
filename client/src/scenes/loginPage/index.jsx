3e7b4ede4fdf6c1c4caac997298a633feb52336a
Eylon <eylon.perets@gmail.com>
2023-01-08 13:42:36 +0200

    login and register forms

--- client/src/scenes/loginPage/index.jsx
+++ client/src/scenes/loginPage/index.jsx
@@ -1,4 +1,30 @@
+import { Box, Typography, useTheme, useMediaQuery } from '@mui/material';
+import Form from './Form';
+
 const LoginPage = () => {
-  return <div>LoginPage</div>;
+  const theme = useTheme();
+  const isNonMobileScreens = useMediaQuery('(min-width:1000px)');
+  const background = theme.palette.background.alt;
+  return (
+    <Box>
+      <Box width="100%" backgroundColor={background} textAlign="center" p="1rem 6%">
+        <Typography fontWeight="bold" fontSize="32px" color="primary">
+          Twitterish
+        </Typography>
+      </Box>
+      <Box
+        width={isNonMobileScreens ? '50%' : '93%'}
+        p="2rem"
+        m="2rem auto"
+        borderRadius="1.5rem"
+        backgroundColor={background}
+      >
+        <Typography fontWeight="500" variant="h5" sx={{ mb: '1.5rem' }}>
+          Welcome to Twitterish, a some-what clone of Twitter!
+        </Typography>
+        <Form />
+      </Box>
+    </Box>
+  );
 };
 export default LoginPage;
--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -9,7 +9,7 @@ import {
   useTheme,
   useMediaQuery,
 } from '@mui/material';
-import { Search, Message, Darkmode, LightMode, Notifications, Help, Menu, Close } from '@mui/icons-material';
+import { Search, Message, Darkmode, LightMode, Notifications, Help, Menu, Close, DarkMode } from '@mui/icons-material';
 import { useDispatch, useSelector } from 'react-redux';
 import { setMode, setLogout } from 'state';
 import { useNavigate } from 'react-router-dom';
@@ -25,11 +25,11 @@ const Navbar = () => {
   const theme = useTheme();
   const neutralLight = theme.palette.neutral.light;
   const dark = theme.palette.neutral.dark;
-  const background = theme.palette.default;
+  const background = theme.palette.background.default;
   const primaryLight = theme.palette.primary.light;
   const alt = theme.palette.background.alt;
 
-  const fullName = `${user.firstName} ${user.lastName}`;
+  const fullName = `${user?.firstName} ${user?.lastName}`;
 
   return (
     <FlexBetween padding="1rem 6%" backgroundColor={alt}>
@@ -42,6 +42,7 @@ const Navbar = () => {
             navigate('/home');
           }}
           sx={{
+            transition: 'color 0.3s',
             '&:hover': {
               color: primaryLight,
               cursor: 'pointer',
@@ -51,14 +52,124 @@ const Navbar = () => {
           Twitterish
         </Typography>
         {isNonMobileScreens && (
-          <FlexBetween
-            backgroundColor={neutralLight}
-            borderRadius="9px"
-            gap="3rem"
-            padding="0.1rem 1.5rem"
-          ></FlexBetween>
+          <FlexBetween backgroundColor={neutralLight} borderRadius="9px" gap="3rem" padding="0.1rem 1.5rem">
+            <InputBase placeholder="Search..." />
+            <IconButton>
+              <Search />
+            </IconButton>
+          </FlexBetween>
         )}
       </FlexBetween>
+      {/* DESKTOP NAV */}
+      {isNonMobileScreens ? (
+        <FlexBetween gap="2rem">
+          <IconButton onClick={() => dispatch(setMode())}>
+            {theme.palette.mode === 'dark' ? (
+              <DarkMode sx={{ fontSize: '25px' }} />
+            ) : (
+              <LightMode sx={{ color: dark, fontSize: '25px' }} />
+            )}
+          </IconButton>
+          <Message sx={{ fontSize: '25px' }} />
+          <Notifications sx={{ fontSize: '25px' }} />
+          <Help sx={{ fontSize: '25px' }} />
+          <FormControl variant="standard" value={fullName}>
+            <Select
+              value={fullName}
+              sx={{
+                backgroundColor: neutralLight,
+                width: '150px',
+                borderRadius: '0.25rem',
+                p: '0.25rem 1rem',
+                '& .MuiSvgIcon-root': { pr: '0.25rem', width: '3rem' },
+                '& .MuiSelect-select:focus': { backgroundColor: neutralLight },
+              }}
+              input={<InputBase />}
+            >
+              <MenuItem value={fullName}>
+                <Typography>{fullName}</Typography>
+              </MenuItem>
+              <MenuItem
+                onClick={() => {
+                  dispatch(setLogout);
+                }}
+              >
+                Log Out
+              </MenuItem>
+            </Select>
+          </FormControl>
+        </FlexBetween>
+      ) : (
+        <IconButton
+          onClick={() => {
+            setIsMobileMenuToggled(!isMobileMenuToggled);
+          }}
+        >
+          <Menu />
+        </IconButton>
+      )}
+      {/* MOBILE NAV */}
+      {!isNonMobileScreens && isMobileMenuToggled && (
+        <Box
+          position="fixed"
+          right="0"
+          bottom="0"
+          height="100%"
+          zIndex="10"
+          maxWidth="500px"
+          minWidth="300px"
+          backgroundColor={background}
+        >
+          {/* CLOSE ICON */}
+          <Box display="flex" justifyContent="flex-end" p="1rem">
+            <IconButton
+              onClick={() => {
+                setIsMobileMenuToggled(!isMobileMenuToggled);
+              }}
+            >
+              <Close />
+            </IconButton>
+          </Box>
+          {/* MENU ITEMS */}
+          <FlexBetween display="flex" flexDirection="column" justifyContent="center" alignItems="center" gap="3rem">
+            <IconButton onClick={() => dispatch(setMode())} sx={{ fontSize: '25px' }}>
+              {theme.palette.mode === 'dark' ? (
+                <DarkMode sx={{ fontSize: '25px' }} />
+              ) : (
+                <LightMode sx={{ color: dark, fontSize: '25px' }} />
+              )}
+            </IconButton>
+            <Message sx={{ fontSize: '25px' }} />
+            <Notifications sx={{ fontSize: '25px' }} />
+            <Help sx={{ fontSize: '25px' }} />
+            <FormControl variant="standard" value={fullName}>
+              <Select
+                value={fullName}
+                sx={{
+                  backgroundColor: neutralLight,
+                  width: '150px',
+                  borderRadius: '0.25rem',
+                  p: '0.25rem 1rem',
+                  '& .MuiSvgIcon-root': { pr: '0.25rem', width: '3rem' },
+                  '& .MuiSelect-select:focus': { backgroundColor: neutralLight },
+                }}
+                input={<InputBase />}
+              >
+                <MenuItem value={fullName}>
+                  <Typography>{fullName}</Typography>
+                </MenuItem>
+                <MenuItem
+                  onClick={() => {
+                    dispatch(setLogout);
+                  }}
+                >
+                  Log Out
+                </MenuItem>
+              </Select>
+            </FormControl>
+          </FlexBetween>
+        </Box>
+      )}
     </FlexBetween>
   );
 };
