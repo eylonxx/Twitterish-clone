f8d0643fadb1d108b4df98e0c0606235b272ce16
Eylon <eylon.perets@gmail.com>
2023-01-10 20:58:43 +0200

    skeleton spacings

--- client/src/components/UserWidgetSkeletonLoader.jsx
+++ client/src/components/UserWidgetSkeletonLoader.jsx
@@ -5,21 +5,26 @@ import WidgetWrapper from './WidgetWrapper';
 const UserWidgetSkeletonLoader = () => {
   return (
     <WidgetWrapper p="0.5rem">
-      <Box gap="0.625rem" pb="1.5rem" display="flex" flexDirection="column">
-        <Box display="flex" flexDirection="row" alignItems="center">
+      <Box gap="0.4rem" pb="1rem" display="flex" flexDirection="column">
+        <Box display="flex" flexDirection="row" alignItems="center" pb="0.8rem">
           <Skeleton variant="circular" width={60} height={60} />
-          <Skeleton variant="rounded" width={190} height={40} />
+          <Skeleton variant="rounded" width="80%" height="40px" />
         </Box>
         <Divider />
-        <Box display="flex" flexDirection="column" gap="1rem">
-          <Skeleton variant="text" sx={{ fontSize: '1rem' }} />
-          <Skeleton variant="text" sx={{ fontSize: '1rem' }} />
+        <Box display="flex" flexDirection="column" gap="0.5rem">
+          <Skeleton variant="text" sx={{ fontSize: '1.5rem' }} />
+          <Skeleton variant="text" sx={{ fontSize: '1.5rem' }} />
         </Box>
         <Divider />
-        <Box display="flex" flexDirection="column" gap="1rem">
-          <Skeleton variant="rounded" width={240} height={40} />
-          <Skeleton variant="rounded" width={240} height={60} />
-          <Skeleton variant="rounded" width={240} height={60} />
+        <Box display="flex" flexDirection="column" gap="0.5rem" mt="1rem">
+          <Skeleton variant="text" sx={{ fontSize: '1.2rem' }} />
+          <Skeleton variant="text" sx={{ fontSize: '1.2rem' }} />
+        </Box>
+        <Divider />
+        <Box display="flex" flexDirection="column" gap="1rem" mt="0.5rem">
+          <Skeleton variant="text" sx={{ fontSize: '1.2rem' }} />
+          <Skeleton variant="rounded" width="100%" height="50px" />
+          <Skeleton variant="rounded" width="100%" height="50px" />
         </Box>
       </Box>
     </WidgetWrapper>
